793a0f1f81fa5869eb26f0df6acb6069596ebb83
Aryeh Leib Taurog <git@aryehleib.com>
2021-05-03 22:34:41 +0300

    refactor: loop with mutable deques
    more idiomatic than recursion with immutable lists

--- src/stackvm.rs
+++ src/stackvm.rs
@@ -33,73 +33,65 @@ fn err_underflow<'a, T>(op: &str) -> Result<T, String> {
 }
 
 fn execute<'a>(stack: Stack, program: Program) -> Result<StackVal, String> {
-    if stack.is_empty() & program.is_empty() {
-        return Ok(StackVal::Void);
-    } else if program.is_empty() {
-        let val = stack[0].clone();
-        return Ok(val);
-    }
-    let mut newprogram = program.clone();
-    let op = newprogram.pop_front().unwrap();
-    let mut newstack = stack.clone();
-    let len = newstack.len();
-    match op {
-        StackExp::PushI(x) => {
-            newstack.push_front(StackVal::IVal(x));
-            execute(newstack, newprogram)
-        },
-        StackExp::PushB(x) => {
-            newstack.push_front(StackVal::BVal(x));
-            execute(newstack, newprogram)
-        },
-        StackExp::Add if len < 2 => { err_underflow("Add") },
-        StackExp::Add => {
-            let a = newstack.pop_front().unwrap();
-            let b = newstack.pop_front().unwrap();
-            match (a, b) {
-                (StackVal::IVal(ia), StackVal::IVal(ib)) => {
-                    newstack.push_front(StackVal::IVal(ia + ib));
-                    execute(newstack, newprogram)
-                },
-                _ => err_type("Add"),
-            }
-        },
-        StackExp::Mul if len < 2 => { err_underflow("Mul") },
-        StackExp::Mul => {
-            let a = newstack.pop_front().unwrap();
-            let b = newstack.pop_front().unwrap();
-            match (a, b) {
-                (StackVal::IVal(ia), StackVal::IVal(ib)) => {
-                    newstack.push_front(StackVal::IVal(ia * ib));
-                    execute(newstack, newprogram)
-                },
-                _ => err_type("Mul"),
-            }
-        },
-        StackExp::And if len < 2 => { err_underflow("And") },
-        StackExp::And => {
-            let a = newstack.pop_front().unwrap();
-            let b = newstack.pop_front().unwrap();
-            match (a, b) {
-                (StackVal::BVal(ba), StackVal::BVal(bb)) => {
-                    newstack.push_front(StackVal::BVal(ba & bb));
-                    execute(newstack, newprogram)
-                },
-                _ => err_type("And"),
-            }
-        },
-        StackExp::Or if len < 2 => { err_underflow("Or") },
-        StackExp::Or => {
-            let a = newstack.pop_front().unwrap();
-            let b = newstack.pop_front().unwrap();
-            match (a, b) {
-                (StackVal::BVal(ba), StackVal::BVal(bb)) => {
-                    newstack.push_front(StackVal::BVal(ba | bb));
-                    execute(newstack, newprogram)
-                },
-                _ => err_type("Or"),
-            }
-        },
+    let mut program = program.clone();
+    let mut stack = stack.clone();
+    loop {
+        if stack.is_empty() & program.is_empty() {
+            return Ok(StackVal::Void);
+        } else if program.is_empty() {
+            let val = stack[0].clone();
+            return Ok(val);
+        }
+        let op = program.pop_front().unwrap();
+        let len = stack.len();
+        match op {
+            StackExp::PushI(x) => { stack.push_front(StackVal::IVal(x)); },
+            StackExp::PushB(x) => { stack.push_front(StackVal::BVal(x)); },
+            StackExp::Add if len < 2 => { return err_underflow("Add"); },
+            StackExp::Add => {
+                let a = stack.pop_front().unwrap();
+                let b = stack.pop_front().unwrap();
+                match (a, b) {
+                    (StackVal::IVal(ia), StackVal::IVal(ib)) => {
+                        stack.push_front(StackVal::IVal(ia + ib));
+                    },
+                    _ => { return err_type("Add"); },
+                }
+            },
+            StackExp::Mul if len < 2 => { return err_underflow("Mul"); },
+            StackExp::Mul => {
+                let a = stack.pop_front().unwrap();
+                let b = stack.pop_front().unwrap();
+                match (a, b) {
+                    (StackVal::IVal(ia), StackVal::IVal(ib)) => {
+                        stack.push_front(StackVal::IVal(ia * ib));
+                    },
+                    _ => { return err_type("Mul"); },
+                }
+            },
+            StackExp::And if len < 2 => { return err_underflow("And"); },
+            StackExp::And => {
+                let a = stack.pop_front().unwrap();
+                let b = stack.pop_front().unwrap();
+                match (a, b) {
+                    (StackVal::BVal(ba), StackVal::BVal(bb)) => {
+                        stack.push_front(StackVal::BVal(ba & bb));
+                    },
+                    _ => { return err_type("And"); },
+                }
+            },
+            StackExp::Or if len < 2 => { return err_underflow("Or"); },
+            StackExp::Or => {
+                let a = stack.pop_front().unwrap();
+                let b = stack.pop_front().unwrap();
+                match (a, b) {
+                    (StackVal::BVal(ba), StackVal::BVal(bb)) => {
+                        stack.push_front(StackVal::BVal(ba | bb));
+                    },
+                    _ => { return err_type("Or"); },
+                }
+            },
+        }
     }
 }
 
@@ -109,9 +101,37 @@ mod stackvm_tests {
     use StackExp::*;
 
     #[test]
-    fn test_stackvm() {
+    fn test_stackvm_simple_add() {
         let program = vec![PushI(3), PushI(5), Add];
         let result = stackvm(VecDeque::from(program));
         assert_eq!(result, Ok(StackVal::IVal(8)));
+    }
+
+    #[test]
+    fn test_stackvm_simple_mul() {
+        let program = vec![PushI(3), PushI(5), Mul];
+        let result = stackvm(VecDeque::from(program));
+        assert_eq!(result, Ok(StackVal::IVal(15)));
+    }
+
+    #[test]
+    fn test_stackvm_add_err_type() {
+        let program = vec![PushB(true), PushB(true), Add];
+        let result = stackvm(VecDeque::from(program));
+        assert_eq!(result, Err(String::from("Encountered 'Add' opcode with ill-typed stack.")));
+    }
+
+    #[test]
+    fn test_stackvm_add_underflow() {
+        let program = vec![PushI(3), Add];
+        let result = stackvm(VecDeque::from(program));
+        assert_eq!(result, Err(String::from("Stack underflow with 'Add' opcode.")));
+    }
+
+    #[test]
+    fn test_stackvm_simple_and() {
+        let program = vec![PushB(true), PushB(true), And];
+        let result = stackvm(VecDeque::from(program));
+        assert_eq!(result, Ok(StackVal::BVal(true)));
     }
 }
